bcfb2aaf669df5d1904b1f15587ea67a43b1b36c
Shane Segui <shanesegui@Shanes-iMac.local>
2021-02-18 16:01:59 +0900

    commit new

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,19 @@ import { setUserParam } from '../redux/';
 import { getData } from '../server/';
 import classes from './Home.module.css';
 
+
+const getUniqueChar = () => {
+    const chars = 'abcdefghijklmnopqrstuvwxyz';
+    const index = Math.floor(chars.length * Math.random());
+    return 'abcdefghijklmnopqrstuvwxyz'.charAt(index)
+}
+
+const getUniqueInt = () => {
+    const tmp = Math.floor(100 * Math.random());
+    return tmp < 10 ? '0' + tmp : tmp;
+}
+
+
 class Page extends React.Component {
 
     constructor(props) {
@@ -16,21 +29,28 @@ class Page extends React.Component {
             items: [],
             redirect: false,
             error: false,
+            errorMessage: "",
         }
 
         this.getList = this.getList.bind(this);
+        this.reloadApp = this.reloadApp.bind(this);
 
     }
 
     componentDidMount() {
 
+        console.log("mounted...")
+
         const { token } = this.props.user;
         if(token.length === 0) {
+
+            console.log("no token. redirecting...")
 
             this.setState({
                 loading: 1,
                 redirect: true,
             })
+
             return;
 
         }
@@ -41,10 +61,26 @@ class Page extends React.Component {
 
     getList() {
 
+        /*
+        var d1 = [];
+        for(var k = 0; k < 3; k++) {
+            var ch = '';
+            for(var i = 0; i < 3; i++) {
+                ch += getUniqueChar();
+            }
+            var suffix = getUniqueInt();
+            d1.push(ch + suffix);
+        }
+
+        console.log("keys...", d1.join('-'));
+        */
+
         getData({ token: this.props.user.token }).then(data => {
             
             const status = typeof data.status !== "undefined" ? parseInt(data.status) : 0;
             if(status === 200) {
+
+                console.log("data received...")
 
                 this.setState({
                     items: data.items,
@@ -54,27 +90,49 @@ class Page extends React.Component {
 
             } else if(status == 401){ // token expired
 
+                console.log("token expired...");
+
                 this.setState({
                     loading: 1,
-                    redirect: true,
+                    error: true,
+                    errorMessage: "Token expired. Please reload the page.",
+                    //redirect: true,
                 })
-                return;
 
             } else {
+
+                console.log("error occurred...");
+
                 this.setState({
                     error: true,
+                    errorMessage: "Unexpected error. Please re-try again.",
                     loading: 1,
                 })
             }
 
         }).catch(error => {
+
             console.log(error);
 
             this.setState({
                 error: true,
+                errorMessage: "Unexpected error. Server problem.",
                 loading: 1,
             })
         })
+
+    }
+
+    reloadApp() {
+        
+        this.setState({
+            items: [],
+            loading: 2,
+        })
+
+        setTimeout(() => {
+            this.getList();
+        }, 1200);
 
     }
 
@@ -88,20 +146,27 @@ class Page extends React.Component {
 
         return (
             <div className={classes.container}>
+                <div onClick={this.reloadApp} className={classes.reload}><span>&#8635;</span></div>
                 {
-                    this.state.error &&
-                    <div className={classes.error}>
-                        <span>Unexpected error. Cannot get data from the server.</span>
+                    this.state.loading === 2 &&
+                    <div className={classes.loader}>
+                        <div className={classes.cycle}><span>&#10561;</span></div>
                     </div>
                 }
                 {
-                    (!this.state.error && this.state.items.length === 0)&&
+                    (this.state.loading === 1 && this.state.error) &&
+                    <div className={classes.error}>
+                        <span>{ this.state.errorMessage }</span>
+                    </div>
+                }
+                {
+                    (this.state.loading === 1 && !this.state.error && this.state.items.length === 0)&&
                     <div className={classes.empty}>
                         <span>Data not found.</span>
                     </div>
                 }
                 {
-                    (!this.state.error && this.state.items.length > 0)&&
+                    (this.state.loading === 1 && !this.state.error && this.state.items.length > 0)&&
                     <div className={classes.list}>
                         <table>
                             <thead>
--- src/server/index.js
+++ src/server/index.js
@@ -5,24 +5,38 @@ const server_config = [
     { key: 'production', host: 'YOUR-IP-ADDRESS', port: 80 },
 ]
 
+const header = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+}
+
+const request = {
+    url: '',
+    method: 'POST',
+    headers: null,
+    payload: null,
+}
+
 export async function submitLogin({ login, password }) 
 {
     
     const config = server_config.find(item => item.key === process.env.NODE_ENV);
     const url = process.env.NODE_ENV === "production" ? `https://${config.host}/test/login.php` : `http://${config.host}:${config.port}/test/login`
     
+    const my_header = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'x-api-key': process.env.REACT_APP_API_KEY,
+    }
+
     let response = await fetch(url, {
         method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'x-api-key': process.env.REACT_APP_API_KEY,
-        },
+        headers: process.env.NODE_ENV === "production" ? my_header : header,
         body: JSON.stringify({
             id: login,
             pwd: password,
         })
-    });
+    })
 
     let result = await response.json();
     return result;
@@ -33,15 +47,59 @@ export async function getData({ token })
     const config = server_config.find(item => item.key === process.env.NODE_ENV);
     const url = process.env.NODE_ENV === "production" ? `https://${config.host}/test/list.php` : `http://${config.host}:${config.port}/test/list`
     
+    const my_header = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Authorization': 'Bearer ' + token,
+    }
+
+    // save
+    request.url = url;
+    request.method = 'GET';
+    request.headers = process.env.NODE_ENV === "production" ? my_header : header;
+
     let response = await fetch(url, {
         method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'Authorization': 'Bearer ' + token,
-        }
-    });
+        headers: process.env.NODE_ENV === "production" ? my_header : header,
+    })
 
     let result = await response.json();
-    return result;
+
+    //console.log("data...", result);
+
+    const status = typeof result.status !== "undefined" ? parseInt(result.status) : 0;
+    if(status === 401) {
+
+        console.log("refresh token...");
+
+        // refresh token
+        //return result;
+
+        const url1 = `http://${config.host}:${config.port}/test/refresh/`;
+        let responseT = await fetch(url1);
+        let resultT = await responseT.json();
+
+        const statusT = typeof resultT.status !== "undefined" ? parseInt(resultT.status) : 0;
+        if(statusT === 401) {
+
+            console.log("...expired");
+
+            return resultT;
+            
+        } else {
+
+            console.log("...okay");
+
+            response = await fetch(url);
+            result = await response.json();
+            return result;
+
+        }
+
+    } else {
+
+        return result;
+
+    }
+    
 }
